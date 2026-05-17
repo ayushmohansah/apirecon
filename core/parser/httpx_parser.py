a0569427bef8868823754298e32635022d462960
Ayush Mohan Sah <71077667+ayushmohansah@users.noreply.github.com>
2026-05-17 03:36:49 -0700

    Improve HTTPX parser using JSON lines

--- core/parser/httpx_parser.py
+++ core/parser/httpx_parser.py
@@ -1,36 +1,25 @@
-import re
+import json
 
 class HTTPXParser:
 
     @staticmethod
     def parse(output):
-
         results = []
 
         for line in output.splitlines():
 
-            result = {
-                "raw": line,
-                "url": None,
-                "status_code": None,
-                "title": None,
-                "technologies": []
-            }
+            try:
+                data = json.loads(line)
 
-            url_match = re.search(r'(https?://[^\s]+)', line)
-            status_match = re.search(r'\[(\d{3})\]', line)
+                results.append({
+                    'url': data.get('url'),
+                    'status_code': data.get('status_code'),
+                    'title': data.get('title'),
+                    'technologies': data.get('tech', []),
+                    'source': 'httpx'
+                })
 
-            if url_match:
-                result["url"] = url_match.group(1)
-
-            if status_match:
-                result["status_code"] = int(status_match.group(1))
-
-            tech_matches = re.findall(r'\[(.*?)\]', line)
-
-            if tech_matches:
-                result["technologies"] = tech_matches
-
-            results.append(result)
+            except Exception:
+                continue
 
         return results
